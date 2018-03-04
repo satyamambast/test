ea41884cfd9b78083807b7ad903bb8d42616f6a3
satyamambast <36452790+satyamambast@users.noreply.github.com>
2018-03-04 21:14:25 +0530

    lx
    adk

--- sketch_mar04a/sketch_mar04a.ino
+++ sketch_mar04a/sketch_mar04a.ino
@@ -1,5 +1,6 @@
 long duration,distance;
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(4, OUTPUT);
 pinMode(12, INPUT);
 Serial.begin(9600); 
@@ -7,13 +8,24 @@ Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(4, LOW);
+digitalWrite(4, LOW); //Output to pin 4: 0
 delayMicroseconds(2);
-digitalWrite(4, HIGH);
+digitalWrite(4, HIGH); //Output to pin4 
 delayMicroseconds(10);
 digitalWrite(4, LOW);
 duration = pulseIn(12, HIGH);
 distance= duration*0.034/2;
-Serial.print("Distance: ");
+Serial.println("Distance: ");
 Serial.println(distance);
+
+if(distance<15)
+{
+  Serial.print("Condition Fulfilled");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delayMicroseconds(2000);
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  
+}
+delayMicroseconds(10);
 }
